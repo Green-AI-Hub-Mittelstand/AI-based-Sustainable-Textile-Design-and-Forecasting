f0876d0271a9a9d4fe7681d61696d8d5a68255d6
Dimitrij Trofimovic <Dimitrij.Trofimovic@dfki.de>
2024-03-03 22:42:30 +0100

    added new rules to data json and simplified tiles

--- src/app/components/clothing/jeans/JeansComponentTile2.tsx
+++ src/app/components/clothing/jeans/JeansComponentTile2.tsx
@@ -1,8 +1,17 @@
+import JeansComponentTileRules from "./JeansComponentRules";
+
 export default function JeansComponentTile2(props: {
   data: any;
   valueRecyclingfähigkeit: number;
   valueLanglebigkeit: number;
   valueRegenerativität: number;
 }) {
-  return <div>{props.data}</div>;
+  return (
+    <JeansComponentTileRules
+      data={props.data}
+      valueRecyclingfähigkeit={props.valueRecyclingfähigkeit}
+      valueLanglebigkeit={props.valueLanglebigkeit}
+      valueRegenerativität={props.valueRegenerativität}
+    ></JeansComponentTileRules>
+  );
 }
