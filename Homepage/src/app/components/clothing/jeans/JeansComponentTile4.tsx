4a8f179801cb43f5ae84000bce36f678d209a07b
Jan Busch <jan.busch@dfki.de>
2024-08-13 08:27:16 +0000

    Upload New File

---
+++ Homepage/src/app/components/clothing/jeans/JeansComponentTile4.tsx
@@ -0,0 +1,24 @@
+export default function JeansComponentTile4(props: {
+  data: any;
+  valueRecyclingfähigkeit: number;
+  valueLanglebigkeit: number;
+  valueRegenerativität: number;
+}) {
+  return (
+    <div style={{ whiteSpace: "pre-line" }}>
+      {props.valueRecyclingfähigkeit <= 2 &&
+        props.valueLanglebigkeit <= 4 &&
+        props.valueRegenerativität === 1
+        ? props.data.case1.text
+        : props.valueRecyclingfähigkeit === 3 &&
+          props.valueLanglebigkeit <= 4 &&
+          props.valueRegenerativität === 1
+          ? props.data.case2.text
+          : props.valueRecyclingfähigkeit === 1 &&
+            props.valueLanglebigkeit <= 4 &&
+            props.valueRegenerativität === 0
+            ? props.data.case3.text
+            : props.data.standardcase.text}
+    </div>
+  );
+}
